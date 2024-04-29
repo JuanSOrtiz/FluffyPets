46d34b687367f9686a2ae8293d228529f7be5d48
Juan <juans.ortiz@udea.edu.co>
2024-04-29 07:53:35 -0500

    primer commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='font-bold text-3xl'>
+        Hola mundo
+      </div>
+    </>
+  )
+}
+
+export default App
