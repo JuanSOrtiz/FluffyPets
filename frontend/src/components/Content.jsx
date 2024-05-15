45d0dbd2795fd1897d8ef5e202d01b8f1fba697e
PaulinaG27 <paulina.garcia1@udea.edu.co>
2024-05-15 18:19:47 -0500

    cambio en los botones de content y logo

--- frontend/src/components/Content.jsx
+++ frontend/src/components/Content.jsx
@@ -16,7 +16,7 @@ const Content = () => {
             <div className="text-center mb-6">
               <h3 className="font-semibold text-xl">Dog Walking</h3>
               <p className="text-gray-700 mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right.</p>
-              <a href="#" className="btn-custom inline-flex items-center justify-center bg-green-500 text-white rounded-full w-12 h-12 shadow-md transition duration-300"><FontAwesomeIcon icon={faChevronRight} /></a>
+              <a href="#" className="btn-custom inline-flex items-center justify-center bg-green-500 text-white rounded-full w-12 h-12 shadow-md transition duration-300 hover:bg-white hover:text-green-500"><FontAwesomeIcon icon={faChevronRight} /></a>
             </div>
           </div>  
           <div className="services-2 relative bg-white shadow rounded-lg overflow-hidden transition duration-300 hover:bg-green-500 hover:text-white">
@@ -26,7 +26,7 @@ const Content = () => {
             <div className="text-center mb-6">
               <h3 className="font-semibold text-xl">Pet Daycare</h3>
               <p className="text-gray-700 mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right.</p>
-              <a href="#" className="btn-custom inline-flex items-center justify-center bg-green-500 text-white rounded-full w-12 h-12 shadow-md transition duration-300"><FontAwesomeIcon icon={faChevronRight} /></a>
+              <a href="#" className="btn-custom inline-flex items-center justify-center bg-green-500 text-white rounded-full w-12 h-12 shadow-md transition duration-300 hover:bg-white hover:text-green-500"><FontAwesomeIcon icon={faChevronRight} /></a>
             </div>
           </div>  
         </div>
